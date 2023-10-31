05c6bc73870b6400b49aa0f3ab84a9ffbe9db42c
Sandipshrestha1 <Sandipshrestha2050@gmail.com>
2023-10-31 12:40:08 +0545

    main page completed

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DictionaryApp());
+}
+class DictionaryApp extends StatelessWidget {
+  const DictionaryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
