0a28e0564a5fd2d0483cbd4b2022d7a32d690859
Sandipshrestha1 <Sandipshrestha2050@gmail.com>
2023-11-04 08:46:22 +0545

    saturday commited

--- lib/cubit/dictionary_cubit.dart
+++ lib/cubit/dictionary_cubit.dart
@@ -1,19 +1,49 @@
 import 'package:dictionary_app/repo/word_repo.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DictionaryCubit extends Cubit<DictionaryState> {
-final WordRepository _repository;
+  final WordRepository _repository;
 
   DictionaryCubit(this._repository) : super(NoWordSearchedState());
+
+  final queryController = TextEditingController();
+
+  Future getWOrdSearched() async {
+    emit(WordSearchingState());
+
+    try {
+  final words =
+      await _repository.getWordsFromDictionary(queryController.text);
+  if (words == null) {
+    emit(ErrorState());
+  } else {
+    WordSearchedState(words);
+  }
+} on Exception catch (e) {
+
+print(e);
+emit(ErrorState());
 }
+  }
+}
+
 abstract class DictionaryState {}
+
 class NoWordSearchedState extends DictionaryState {}
+
 class WordSearchingState extends DictionaryState {}
-class WordSearchedState extends DictionaryState{
+
+class WordSearchedState extends DictionaryState {
   final List<WordResponse> words;
 
   WordSearchedState(this.words);
 }
 
+class ErrorState extends DictionaryState {
 
-class ErrorState extends DictionaryState {}
+
+
+
+
+}
