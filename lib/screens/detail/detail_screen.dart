5b0879ac2ace79d687ec22cfda31623a88e295f2
Sandipshrestha1 <Sandipshrestha2050@gmail.com>
2023-11-06 19:22:28 +0545

    monday updated 2

---
+++ lib/screens/detail/detail_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  // const DetailScreen({super.key});
+
+  final WordResponse wordResponse;
+
+  DetailScreen(this.wordResponse);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      body: Container(
+        child: Column(
+          children: [
+            Text(
+              "${wordResponse.word}",
+              style: TextStyle(color: Colors.white, fontSize: 36),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+Expanded (
+child: ListView.separated(
+  itemBuilder: (context, index) {
+
+  final meaning = WordResponse.meanings[index];
+  final definations = meaning.definitions;
+  return Column(
+crossAxisAlignment: CrossAxisAlignment.start,
+
+children: [
+  Text(meaning.partOfSpeech, style: TextStyle(color: Colors.white),),
+  SizedBox(height: 8,),
+  ListView.separated(itemBuilder: (context, index) => Column(children: [
+Text("Defination: " + definations[index].defination),
+SizedBox(height: 4,),
+Text("Sentence:  ${definations[index].example}") ,  
+  ],
+  crossAxisAlignment: CrossAxisAlignment.start,
+  ),
+   separatorBuilder: (context, index) =>SizedBox(height: 20,),
+   itemCount: definations.length,
+   shrinkWrap: true,
+  )
+],
+
+
+
+  );
+
+
+}, itemCount: wordResponse.meanings.length,
+separatorBuilder: (context, index) => SizedBox(height: 32,), )
+
+
+
+),
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/list/list_screen.dart
+++ lib/screens/list/list_screen.dart
@@ -1,11 +1,43 @@
+// import 'package:dictionary_app/screens/detail/detail_screen.dart';
+// import 'package:flutter/material.dart';
+
+// class ListScreen extends StatelessWidget {
+//   const ListScreen({super.key});
+
+//   final List<WordResponse> words;
+
+//   ListScreen(this.words);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.blueGrey[900],
+//       body: ListView.separated(
+//         itemBuilder: (context, index) => ListTile(
+//           title: Text("${index+1}. ${words[index].word }" ,style: TextStyle(color: Colors.white)),
+        
+//         onTap: () {
+//           Navigator.push(context, MaterialPageRoute(builder: (context) => DetailScreen(words[index])),);
+//         },
+        
+//         ),
+//         separatorBuilder: (context, index) => Divider(
+//           color: Colors.grey,
+//         ),
+//         itemCount: words.length,
+//       ),
+//     );
+//   }
+// }
+
+
+import 'package:dictionary_app/screens/detail/detail_screen.dart';
 import 'package:flutter/material.dart';
 
 class ListScreen extends StatelessWidget {
-  ListScreen({super.key});
-
   final List<WordResponse> words;
 
-  ListScreen(this.words);
+  ListScreen({Key? key, required this.words}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +45,15 @@ class ListScreen extends StatelessWidget {
       backgroundColor: Colors.blueGrey[900],
       body: ListView.separated(
         itemBuilder: (context, index) => ListTile(
-          title: Text("${index+1}. ${words[index].word }" ,style: TextStyle(color: Colors.white)),
+          title: Text("${index + 1}. ${words[index].word}", style: TextStyle(color: Colors.white)),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DetailScreen(wordResponse: words[index]),
+              ),
+            );
+          },
         ),
         separatorBuilder: (context, index) => Divider(
           color: Colors.grey,
@@ -23,3 +63,4 @@ class ListScreen extends StatelessWidget {
     );
   }
 }
+
